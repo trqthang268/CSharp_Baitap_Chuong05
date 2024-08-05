b9324c114bb3a43bade3d1b0dcc20bca183f5d61
trqthang268 <155518610+trqthang268@users.noreply.github.com>
2024-08-05 17:19:29 +0700

    Update

--- Baitap01-chuong5/Program.cs
+++ Baitap01-chuong5/Program.cs
@@ -1,9 +1,58 @@
-﻿namespace Baitap01_chuong5;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
 
-class Program
+namespace Baitap_Chuong5
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Item[] items = new Item[5];
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new Item();
+                Console.WriteLine($"Nhập thông tin cho món hàng thứ {i + 1}");
+                Console.Write("Mã số : ");
+                items[i].id = Convert.ToInt16(Console.ReadLine());
+
+                Console.Write("Tên hàng : ");
+                items[i].name = Console.ReadLine();
+
+                Console.Write("Giá cả : ");
+                items[i].price = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Số lượng : ");
+                items[i].quantity = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            //hiển thị tên từng món hàng và tổng giá trị của nó
+            double totalWarehouseValue = 0;
+            Console.WriteLine("Thông tin các món hàng có trong kho :");
+            foreach(var item in items)
+            {
+                double itemValue = item.totalValue();
+                Console.WriteLine($"Tên hàng : {item.name}, Tổng giá trị hàng : {itemValue.ToString("C", CultureInfo.CurrentCulture)}");
+                totalWarehouseValue += itemValue;
+            }
+
+            Console.WriteLine($"\nTổng giá trị kho hàng : {totalWarehouseValue.ToString("C", CultureInfo.CurrentCulture)}");
+        }
+    }
+
+    class Item
+    {
+        public int id;
+        public string name;
+        public double price;
+        public int quantity;
+
+        public double totalValue()
+        {
+            return price*quantity;
+        }
     }
 }
+
